9766f9ff66e1e2be5c815a8c3a9aab1b2c3895dc
mavaquero05 <mavaquero05@iesalbarregas.es>
2021-03-01 18:21:25 +0100

    2 Validacion

--- EjercicioGit/src/prueba1/Principal.java
+++ EjercicioGit/src/prueba1/Principal.java
@@ -4,7 +4,12 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a=3,b=5,c=0;
 		System.out.println("Hola mundo");
+		
+		c = a + b;
+		System.out.println("La suma de 5 y 3 es: " + c);
+		
 	}
 
 }
